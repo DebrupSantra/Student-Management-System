e14b032104c1eb4c16e4885af912c55b916e041b
DebrupSantra <debrup.santra@gmail.com>
2026-06-28 21:36:22 +0530

    Added delete and update student features

--- main.cpp
+++ main.cpp
@@ -9,20 +9,27 @@ int main(){
 cout << "1. Add Student\n";
 cout << "2. Display Students\n";
 cout << "3. Search Student\n";
-cout << "4. Exit\n";
+cout << "4. Delete Student\n";
+cout << "5. Update Student\n";
+cout << "6. Exit\n";
 cout << "Enter your choice: ";
 int choice;
 cin >> choice;
 switch(choice){
-        case 1:
+       case 1:
 {
     Student s;
     s.input();
     students.push_back(s);
+
+    cout << "\nStudent added successfully!\n";
+    cout << "Vector size = " << students.size() << endl;
+
     break;
 }
         case 2:
 {
+    cout << "Inside case 2";
     for(int i = 0; i < students.size(); i++)
     {
         students[i].display();
@@ -58,8 +65,57 @@ switch(choice){
 }  
         case 4:
 {
+    int deleteRoll;
+    bool found = false;
+
+    cout << "Enter Roll Number to delete: ";
+    cin >> deleteRoll;
+
+    for(int i = 0; i < students.size(); i++)
+    {
+        if(students[i].getRoll() == deleteRoll)
+        {
+            students.erase(students.begin() + i);
+
+            cout << "Student deleted successfully!\n";
+            found = true;
+            break;
+        }
+    }
+
+    if(!found)
+    {
+        cout << "Student not found!\n";
+    }
+
+    break;
+}
+    case 5:{
+        int updateRoll;
+        bool found=false;
+        cout << "Enter roll number to update: ";
+        cin >> updateRoll;
+        for(int i = 0; i < students.size(); i++)
+{
+    if(students[i].getRoll() == updateRoll)
+    {
+       cout << "\nEnter the new details:\n";
+       students[i].input();
+       cout << "Student details successfully updated: ";
+       found =true;
+       break;
+    }    
+}
+    if(!found){
+        cout << "Student does not exist!\n";
+    }
+    break;
+    }    
+    case 6:
+{
     cout << "Thank you for using Student Management System!\n";
     return 0;
 }
   }
-
+    }
+}
